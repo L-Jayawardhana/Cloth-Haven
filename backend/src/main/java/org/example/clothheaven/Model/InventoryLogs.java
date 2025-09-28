268514501c4bba8a2eebd8c214ee254252e79ccd
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-09-28 12:27:29 +0530

    fixed some bugs after merging with jayaru

--- backend/src/main/java/org/example/clothheaven/Model/InventoryLogs.java
+++ backend/src/main/java/org/example/clothheaven/Model/InventoryLogs.java
@@ -1,8 +1,17 @@
 package org.example.clothheaven.Model;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "inventory_logs")
